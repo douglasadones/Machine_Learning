12b05c24eb6a31ac3ed4777463fe01ec89e0e767
Douglas <contato.douglas.ad@gmail.com>
2024-02-04 11:35:01 -0300

    adicionando lógica backpropagation

--- multilayer/multilayer_web.py
+++ multilayer/multilayer_web.py
@@ -24,6 +24,7 @@ outputs = np.array([
     [0],
 ])
 
+# weights before 1.000.000 learning epochs
 weights0 = np.array([
     [-0.424, -0.740, -0.961],
     [0.358, -0.893, -0.469],
@@ -31,7 +32,28 @@ weights0 = np.array([
 
 weights1 = np.array([[-0.017], [-0.893], [0.148]])
 
-epoch = 100
+# weights after 1.000.000 learning epochs with 99% average accuracy
+# (0.006920605401532326 absolute_error_mean)
+new_weights0 = np.array([
+    [-13.12185181,  -1.02832185,   5.95205043],
+    [5.95404295,  -1.02821446, -13.11618268],
+    ])
+
+new_weights1 = np.array([
+    [16.34428342], 
+    [-42.44674413], 
+    [16.34345593],
+    ])
+
+
+# automatic weights generator. 
+aut_weiths0 = 2 * np.random((2, 3)) - 1
+aut_weiths1 = 2 * np.random((3, 1)) - 1
+# np.random((inputs or hidden_layers, hidden_layers or output_layer))
+# We multiply by 2 and subtract by 1 to generate some negative weights.
+
+
+epoch = 1000000
 learning_rate = 0.3
 momentum = 1
 
@@ -44,7 +66,7 @@ for j in range(epoch):
     output_layer = sigmoid(synapse_sum1)
 
     output_layer_error = outputs - output_layer
-    error_absolute_mean = np.mean(np.abs(output_layer_error))
+    absolute_error_mean = np.mean(np.abs(output_layer_error))
 
     # gradient descent calculus (weight adjustment) ------------------------------
     output_derivative = derivativeSigmoid(output_layer)
@@ -59,3 +81,13 @@ for j in range(epoch):
     transposed_hidden_layer = hidden_layer.T
     new_weights1 = transposed_hidden_layer.dot(output_delta)
     weights1 = (weights1 * momentum) + (new_weights1 * learning_rate)
+
+    transposed_input_layer = input_layer.T
+    new_weights0 = transposed_input_layer.dot(hidden_layer_delta)
+    weights0 = (weights0 * momentum) + (new_weights0 * learning_rate) 
+
+print(absolute_error_mean)
+print()
+print(weights0)
+print()
+print(weights1)
